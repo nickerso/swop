48b1a8a630ebffd524cda552d043b3705b49d05e
David Nickerson <nickerso@users.sourceforge.net>
2017-12-19 17:50:24 +1300

    adding preliminary docker and node files.
    These are the configuration files required to build and deploy a node server in docker
    container which will serve the SWOP pages. These will currently fail due to cross domain
    access to PMR being invalid.

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
